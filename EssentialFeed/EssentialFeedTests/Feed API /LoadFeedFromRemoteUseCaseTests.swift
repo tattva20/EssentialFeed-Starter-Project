10d6677e23ae7020b8cc4192a4ef51d325fd87aa
Octavio Rojas <financieraufc@gmail.com>
2024-10-30 15:29:10 -0600

    Extract duplicate test code into a sherd helper method.

--- EssentialFeed/EssentialFeedTests/Feed API /LoadFeedFromRemoteUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API /LoadFeedFromRemoteUseCaseTests.swift
@@ -143,7 +143,7 @@ final class LoadFeedFromRemoteUseCaseTests: XCTestCase {
         return try! JSONSerialization.data(withJSONObject: json)
     }
     
-    private func expect(_ sut: RemoteFeedLoader, toCompleteWith expectedResult: RemoteFeedLoader.Result, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
+    private func expect(_ sut: RemoteFeedLoader, toCompleteWith expectedResult: RemoteFeedLoader.Result, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
         
         let exp = expectation(description: "Wait for load completion")
         sut.load { receivedResult in
